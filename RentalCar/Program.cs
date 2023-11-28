6ec574134eb825eb5f74ec26d3100b0128294197
buihuybao <88084927+buihuybao@users.noreply.github.com>
2023-11-28 15:12:45 +0700

    update

--- RentalCar/Program.cs
+++ RentalCar/Program.cs
@@ -17,7 +17,7 @@ namespace RentalCar
           
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Main());
+            Application.Run(new startScreen());
         }
     }
 }
